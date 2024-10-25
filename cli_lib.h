760a3c213e069640278887be8930c9d77e04fbc0
Gustavo Carvalho <gustavoferrazcarvalho@gmail.com>
2024-10-25 20:46:57 -0300

    Atualização

--- cli_lib.h
+++
@@ -1,6 +0,0 @@
-#ifndef CLI_LIB_H
-#define CLI_LIB_H
-
-void process_args(int argc, char *argv[]);
-
-#endif
--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "cli_lib.h"
-
-int main(int argc, char *argv[]) {
-    process_args(argc, argv);
-    return 0;
-}
